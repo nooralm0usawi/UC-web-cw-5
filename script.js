25df34d0c474021f466ac67d0099d20d859c25c7
nooralm0usawi <125586146+nooralm0usawi@users.noreply.github.com>
2023-07-31 18:46:20 +0300

    done

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("مرحبًا بكم في موقعي")
+
+let grade = prompt("enter your grade her")
+
+console.log(grade)
+
+if (grade >= 90 && grade <= 100)  {
+    console.log ("لقد حصلت على امتياز 🥳")
+}
+ else if (grade >= 80 && grade <= 89 ) {
+    console.log ("لقد حصلت على جيد جداً🤩")
+} else if (grade >= 70 && grade <= 79 ) {
+    console.log ("لقد حصلت على جيد🙂")
+} else if (grade >= 60 && grade <= 69 ) {
+    console.log ("لقد حصلت على مقبول😕")
+} else if (grade >= 50 && grade <= 59 ) {
+    console.log ("لقد حصلت على ضعيف☹️")
+} else {
+    console.log ("راسب💔")
+} 
+// جافا سكربت ليست جافا
